40878da95ca4041e12a03869b94365cbfea68977
Byul Park <starbloom128@gmail.com>
2018-06-15 09:57:05 +0900

    실행시간 측정하기 2 - 메서드 실행 전후 시간 측정

--- src/main/java/kr/hs/emirim/bstar128/aopcal/cal/RecCalculator.java
+++ src/main/java/kr/hs/emirim/bstar128/aopcal/cal/RecCalculator.java
@@ -4,18 +4,11 @@ public class RecCalculator implements Calculator{
 	
 	@Override
 	public long factorial(long num) {
-		long start=System.currentTimeMillis();
-		try {
+		
 			if (num == 0)
 				return 1;
 			else
 				return num * factorial(num - 1);
-		}finally {
-			long end=System.currentTimeMillis();
-			
-			System.out.printf("Imp factorial{%d}: 실행시간: -> %d",num,(end-start));
-			
-		}
 		
 	}
 }
